79a6a261b9f39a2868c4641a19a531bc27247265
Hans-Dieter Buddenberg Blamey <h.buddenberg@gmail.com>
2025-03-23 23:45:06 -0300

    Corregir la asignación de variables en main.sh para evitar errores de sintaxis.

--- src/modules/post_install/main.sh
+++ src/modules/post_install/main.sh
@@ -10,7 +10,7 @@ fi
 if [ -z "$SCRIPT_DIR" ]; then
     SCRIPT_DIR=$(dirname "$(realpath "$BASH_SOURCE")")
 fi
-$SCRIPT_DIR_ORIGINAL = $SCRIPT_DIR
+SCRIPT_DIR_ORIGINAL=$SCRIPT_DIR
 echo "directorio actual: $SCRIPT_DIR"
 read -p "Presiona Enter para continuar..."
 
@@ -18,7 +18,7 @@ read -p "Presiona Enter para continuar..."
 function configure_system() {
 
     # Ejecutar los comandos directamente dentro del entorno chroot
-    $SCRIPT_DIR = /tmp/ArchLinux_Install
+    $SCRIPT_DIR = "/tmp/ArchLinux_Install"
     arch-chroot /mnt /bin/bash -c "
 pacman --noconfirm -Sy git
 cd /tmp
