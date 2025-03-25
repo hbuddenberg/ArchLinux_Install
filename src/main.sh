82cabdd9e53a28ffa0a6b1e16d5ef29ec68be3d0
Hans Buddenberg <h.buddenberg@gmail.com>
2025-03-25 08:27:20 -0300

    Actualizar el logo de Arch Linux en main.sh para utilizar la variable ARCH_BLUE y mejorar la presentación del menú principal.

--- src/main.sh
+++ src/main.sh
@@ -26,31 +26,31 @@ ARCH_WHITE="\e[1;37m"
 ARCH_RESET="\e[0m"
 
 # Logo de Arch Linux
-ARCH_LOGO="\033[38;2;23;147;209m                   ▄
-\033[38;2;23;147;209m                  ▟█▙
-\033[38;2;23;147;209m                 ▟███▙
-\033[38;2;23;147;209m                ▟█████▙
-\033[38;2;23;147;209m               ▟███████▙
-\033[38;2;23;147;209m              ▂▔▀▜██████▙
-\033[38;2;23;147;209m             ▟██▅▂▝▜█████▙               
-\033[38;2;23;147;209m            ▟█████████████▙              
-\033[38;2;23;147;209m           ▟███████████████▙             
-\033[38;2;23;147;209m          ▟█████████████████▙            \e[1;37m               #     \e[38;2;23;147;209m| *
-\033[38;2;23;147;209m         ▟███████████████████▙           \e[1;37m a##e #%" a#"e 6##%  \e[38;2;23;147;209m| | |-^-. |   | \\ /
-\033[38;2;23;147;209m        ▟█████████▛▀▀▜████████▙          \e[1;37m.oOo# #   #    #  #  \e[38;2;23;147;209m| | |   | |   |  X
-\033[38;2;23;147;209m       ▟████████▛      ▜███████▙         \e[1;37m%OoO# #   %#e" #  #  \e[38;2;23;147;209m| | |   | ^._.| / \\
-\033[38;2;23;147;209m      ▟█████████        ████████▙        
-\033[38;2;23;147;209m     ▟██████████        █████▆▅▄▃▂       
-\033[38;2;23;147;209m    ▟██████████▛        ▜█████████▙      
-\033[38;2;23;147;209m   ▟██████▀▀▀              ▀▀██████▙     
-\033[38;2;23;147;209m  ▟███▀▘                       ▝▀███▙    
-\033[38;2;23;147;209m ▟▛▀                               ▀▜▙   \e[0m
+ARCH_LOGO="${ARCH_BLUE}                   ▄
+${ARCH_BLUE}                  ▟█▙
+${ARCH_BLUE}                 ▟███▙
+${ARCH_BLUE}                ▟█████▙
+${ARCH_BLUE}               ▟███████▙
+${ARCH_BLUE}              ▂▔▀▜██████▙
+${ARCH_BLUE}             ▟██▅▂▝▜█████▙               
+${ARCH_BLUE}            ▟█████████████▙              
+${ARCH_BLUE}           ▟███████████████▙             
+${ARCH_BLUE}          ▟█████████████████▙            ${ARCH_WHITE}               #     ${ARCH_BLUE}| *
+${ARCH_BLUE}         ▟███████████████████▙           ${ARCH_WHITE} a##e #%" a#"e 6##%  ${ARCH_BLUE}| | |-^-. |   | \\ /
+${ARCH_BLUE}        ▟█████████▛▀▀▜████████▙          ${ARCH_WHITE}.oOo# #   #    #  #  ${ARCH_BLUE}| | |   | |   |  X
+${ARCH_BLUE}       ▟████████▛      ▜███████▙         ${ARCH_WHITE}%OoO# #   %#e" #  #  ${ARCH_BLUE}| | |   | ^._.| / \\
+${ARCH_BLUE}      ▟█████████        ████████▙        
+${ARCH_BLUE}     ▟██████████        █████▆▅▄▃▂       
+${ARCH_BLUE}    ▟██████████▛        ▜█████████▙      
+${ARCH_BLUE}   ▟██████▀▀▀              ▀▀██████▙     
+${ARCH_BLUE}  ▟███▀▘                       ▝▀███▙    
+${ARCH_BLUE} ▟▛▀                               ▀▜▙   ${ARCH_RESET}
 "
 
 # Función para mostrar el menú principal
 function show_menu {
     clear
-        gum style --border bold --margin "1" --padding "1" --border-foreground "$ARCH_BLUE" --foreground "$ARCH_WHITE" "echo -e $ARCH_LOGO"
+    gum style --border bold --margin "1" --padding "1" --border-foreground "$ARCH_BLUE" --foreground "$ARCH_WHITE" "echo -e \"$ARCH_LOGO\""
     opcion=$(gum choose --cursor.foreground="$ARCH_BLUE" --selected.foreground="$ARCH_WHITE" \
         "1) Nueva Instalación" \
         "2) Utilidades" \
