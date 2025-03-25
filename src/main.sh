a8ca6db129fc234eb5c2a9b66ed9b91e77e4b9af
Hans Buddenberg <h.buddenberg@gmail.com>
2025-03-24 23:46:01 -0300

    Refactor el logo de Arch Linux en main.sh para mejorar la presentación y la legibilidad del menú principal.

--- src/main.sh
+++ src/main.sh
@@ -7,31 +7,23 @@ HEX_GREEN="#306230"
 HEX_LIGHT_GREEN="#8bac0f"
 HEX_LIGHTEST_GREEN="#9bbc0f"
 
-# Colores Gameboy
-TTY_DARK_GREEN="\e[38;2;15;56;15m"
-TTY_GREEN="\e[38;2;48;98;48m"
-TTY_LIGHT_GREEN="\e[38;2;139;172;15m"
-TTY_LIGHTEST_GREEN="\e[38;2;155;188;15m"
-TTY_CLEAR="\e[H\e[2J"
-TTY_RESET="\e[0m"
-
 # Logo de Arch Linux
 ARCH_LOGO="
-$TTY_CLEAR
-           $TTY_DARK_GREEN.
-          $TTY_DARK_GREEN/#\\
-         $TTY_DARK_GREEN/###\\      $TTY_LIGHTEST_GREEN               #     $TTY_DARK_GREEN| *
-        $TTY_DARK_GREEN/p^###\\     $TTY_LIGHTEST_GREEN a##e #%\" a#\"e 6##%  $TTY_DARK_GREEN| | |-^-. |   | \\ /
-       $TTY_DARK_GREEN/##P^q##\\    $TTY_LIGHTEST_GREEN.oOo# #   #    #  #  $TTY_DARK_GREEN| | |   | |   |  X
-      $TTY_DARK_GREEN/##(   )##\\   $TTY_LIGHTEST_GREEN%OoO# #   %#e\" #  #  $TTY_DARK_GREEN| | |   | ^._.| / \\ $TTY_LIGHT_GREEN TM
-     $TTY_DARK_GREEN/###P   q#,^\\
-    $TTY_DARK_GREEN/P^         ^q\\ $TTY_LIGHT_GREEN TM
+\e[H\e[2J
+           \e[38;2;15;56;15m.
+          \e[38;2;15;56;15m/#\\
+         \e[38;2;15;56;15m/###\\      \e[38;2;155;188;15m               #     \e[38;2;15;56;15m| *
+        \e[38;2;15;56;15m/p^###\\     \e[38;2;155;188;15m a##e #%\" a#\"e 6##%  \e[38;2;15;56;15m| | |-^-. |   | \\ /
+       \e[38;2;15;56;15m/##P^q##\\    \e[38;2;155;188;15m.oOo# #   #    #  #  \e[38;2;15;56;15m| | |   | |   |  X
+      \e[38;2;15;56;15m/##(   )##\\   \e[38;2;155;188;15m%OoO# #   %#e\" #  #  \e[38;2;15;56;15m| | |   | ^._.| / \\ \e[38;2;139;172;15m TM
+     \e[38;2;15;56;15m/###P   q#,^\\
+    \e[38;2;15;56;15m/P^         ^q\\ \e[38;2;139;172;15m TM
 "
 
 # Función para mostrar el menú principal
 function show_menu {
     clear
-    gum style --border normal --margin "1" --padding "1" --border-foreground "$HEX_DARK_GREEN" --foreground "$HEX_LIGHTEST_GREEN" "$ARCH_LOGO"
+    gum style --border normal --margin "1" --padding "1" --border-foreground "$HEX_DARK_GREEN" --foreground "$HEX_LIGHTEST_GREEN" "$(echo -e "$ARCH_LOGO")"
     gum style --border normal --margin "1" --padding "1" --border-foreground "$HEX_DARK_GREEN" --foreground "$HEX_LIGHTEST_GREEN" "ArchHypr Install - Menú Principal"
     opcion=$(gum choose --cursor.foreground="$HEX_GREEN" --selected.foreground="$HEX_LIGHT_GREEN" \
         "1) Nueva Instalación" \
