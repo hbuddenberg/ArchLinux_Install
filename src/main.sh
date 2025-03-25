1068ac1d8d993e8ce5978b1d4b80750eceb7a805
Hans Buddenberg <h.buddenberg@gmail.com>
2025-03-24 23:30:43 -0300

    Actualizar colores y mejorar el logo en main.sh para una mejor presentación del menú principal.

--- src/main.sh
+++ src/main.sh
@@ -2,43 +2,50 @@
 clear
 
 # Colores Gameboy
-DARK_GREEN="\e[38;2;15;56;15m"
-GREEN="\e[38;2;48;98;48m"
-LIGHT_GREEN="\e[38;2;139;172;15m"
-LIGHTEST_GREEN="\e[38;2;155;188;15m"
-RESET="\e[0m"
+HEX_DARK_GREEN="#0f380f"
+HEX_GREEN="#306230"
+HEX_LIGHT_GREEN="#8bac0f"
+HEX_LIGHTEST_GREEN="#9bbc0f"
+
+# Colores Gameboy
+TTY_DARK_GREEN="\e[38;2;15;56;15m"
+TTY_GREEN="\e[38;2;48;98;48m"
+TTY_LIGHT_GREEN="\e[38;2;139;172;15m"
+TTY_LIGHTEST_GREEN="\e[38;2;155;188;15m"
+TTY_CLEAR="\e[H\e[2J"
+TTY_RESET="\e[0m"
 
 # Logo de Arch Linux
 ARCH_LOGO="
-\e[H\e[2J
-           \e[1;36m.
-          \e[1;36m/#\\
-         \e[1;36m/###\\      \e[1;37m               #     \e[1;36m| *
-        \e[1;36m/p^###\\     \e[1;37m a##e #%\" a#\"e 6##%  \e[1;36m| | |-^-. |   | \\ /
-       \e[1;36m/##P^q##\\    \e[1;37m.oOo# #   #    #  #  \e[1;36m| | |   | |   |  X
-      \e[1;36m/##(   )##\\   \e[1;37m%OoO# #   %#e\" #  #  \e[1;36m| | |   | ^._.| / \\ \e[0;37mTM
-     \e[1;36m/###P   q#,^\\
-    \e[1;36m/P^         ^q\\ \e[0;37mTM
+$TTY_CLEAR
+           $TTY_DARK_GREEN.
+          $TTY_DARK_GREEN/#\\
+         $TTY_DARK_GREEN/###\\      $TTY_LIGHTEST_GREEN               #     $TTY_DARK_GREEN| *
+        $TTY_DARK_GREEN/p^###\\     $TTY_LIGHTEST_GREEN a##e #%\" a#\"e 6##%  $TTY_DARK_GREEN| | |-^-. |   | \\ /
+       $TTY_DARK_GREEN/##P^q##\\    $TTY_LIGHTEST_GREEN.oOo# #   #    #  #  $TTY_DARK_GREEN| | |   | |   |  X
+      $TTY_DARK_GREEN/##(   )##\\   $TTY_LIGHTEST_GREEN%OoO# #   %#e\" #  #  $TTY_DARK_GREEN| | |   | ^._.| / \\ $TTY_LIGHT_GREEN TM
+     $TTY_DARK_GREEN/###P   q#,^\\
+    $TTY_DARK_GREEN/P^         ^q\\ $TTY_LIGHT_GREEN TM
 "
 
 # Función para mostrar el menú principal
 function show_menu {
     clear
-    echo -e "$ARCH_LOGO"
-    gum style --border normal --margin "1" --padding "1" --border-foreground "$DARK_GREEN" --foreground "$LIGHTEST_GREEN" "ArchHypr Install - Menú Principal"
-    opcion=$(gum choose --cursor.foreground="$GREEN" --selected.foreground="$LIGHT_GREEN" \
+    gum style --border normal --margin "1" --padding "1" --border-foreground "$HEX_DARK_GREEN" --foreground "$HEX_LIGHTEST_GREEN" "$ARCH_LOGO"
+    gum style --border normal --margin "1" --padding "1" --border-foreground "$HEX_DARK_GREEN" --foreground "$HEX_LIGHTEST_GREEN" "ArchHypr Install - Menú Principal"
+    opcion=$(gum choose --cursor.foreground="$HEX_GREEN" --selected.foreground="$HEX_LIGHT_GREEN" \
         "1) Nueva Instalación" \
         "2) Utilidades" \
         "0) Salir")
     
     case $opcion in
-        "- Nueva Instalación")
+        "1) Nueva Instalación")
             bash "$SCRIPT_DIR/new_install/main.sh"
             ;;
-        "- Utilidades")
+        "2) Utilidades")
             bash "$SCRIPT_DIR/utilities/main.sh"
             ;;
-        "- Salir")
+        "0) Salir")
             echo "Saliendo..."
             exit 0
             ;;
