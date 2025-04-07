61ef2fe8d8c21b1131dba549bfc4e1082172e13d
Hans-Dieter Buddenberg Blamey <h.buddenberg@gmail.com>
2025-04-07 14:19:33 -0400

    Mejorar la opción de uso de Reflector en main.sh al eliminar el uso de colores en las opciones de selección, facilitando la interacción del usuario.

--- src/modules/new_install/pacman_update/main.sh
+++ src/modules/new_install/pacman_update/main.sh
@@ -64,7 +64,7 @@ sed -i '/^#\[multilib\]/{N;s/#\(\[multilib\]\)\n#\(Include = \/etc\/pacman.d\/mi
 gum style --foreground 34 "Cambios aplicados a $PACMAN_CONF"
 
 # Preguntar si se desea usar reflector con un encabezado claro
-USE_REFLECTOR=$(gum choose --header "¿Quieres usar Reflector para optimizar los mirrors de Arch Linux?" --item.foreground="green" "Sí, optimizar mirrors" --item.foreground="red" "No, continuar sin optimizar")
+USE_REFLECTOR=$(gum choose --header "¿Quieres usar Reflector para optimizar los mirrors de Arch Linux?" "Sí, optimizar mirrors" "No, continuar sin optimizar")
 if [[ "$USE_REFLECTOR" == "Sí, optimizar mirrors" ]]; then
     ## Reflector
     DEFAULT_COUNTRY="Chile"
