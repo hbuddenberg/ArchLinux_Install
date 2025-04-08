27bfad13daedc7082a58cfdb176b2925c3e1b4a0
Hans-Dieter Buddenberg Blamey <h.buddenberg@gmail.com>
2025-04-08 16:40:20 -0400

    Mejorar la interacción del usuario en main.sh utilizando gum para confirmar la creación de un usuario adicional.

--- src/modules/new_install/post_install/main.sh
+++ src/modules/new_install/post_install/main.sh
@@ -95,7 +95,7 @@ function ask_user_password() {
 # Función para preguntar si se desea ingresar un usuario
 function ask_for_user_creation() {
     gum style --foreground 10 "¿Deseas crear un usuario adicional?"
-    create_user=$(gum choose --cursor.foreground="212" "Sí" "No")
+    create_user=$(gum confirm --cursor.foreground="212" --affirmative "Sí" --negative "No")
     if [[ "$create_user" == "Sí" ]]; then
         set_username
         ask_user_password
